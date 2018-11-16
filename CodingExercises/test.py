4680aaecfe605390893a8b4f39678ca7dce2def5
Akhand <akhand17@hotmail.com>
2018-11-15 23:55:18 -0800

    local changes

---
+++ CodingExercises/test.py
@@ -0,0 +1,12 @@
+
+def DailyCodingProblem2(ary):
+        print i , ary.count(i)
+
+def main():
+    ary=[1, 2, 3, 4, 4, 5]
+    print DailyCodingProblem2(ary)
+
+
+if __name__ == "__main__":
+    main()
+
