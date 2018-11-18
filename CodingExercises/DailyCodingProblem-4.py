3df13a2709d6d8f846901f86e4a5993adc540497
Akhand <akhand17@hotmail.com>
2018-11-18 13:37:25 -0800

    Daily Coding problems

--- CodingExercises/DailyCodingProblem-4.py
+++ CodingExercises/DailyCodingProblem-4.py
@@ -13,20 +13,40 @@ You can modify the input array in-place.
 
 '''
 
-def firstmissingnum(ary):
-    for i in ary:
-        if i < 0:
-            ary.remove(i)
-        else:
-            continue
+# positive missing number
 
-    for i in range(len(ary)):
-        print ary[(ary[i]-1)]
+def solution(A):#Our original array
+
+    m = max(A) #Storing maximum value
+    if m < 1:
+
+        # In case all values in our array are negative
+        return 1
+    if len(A) == 1:
+
+        #If it contains only one element
+        return 2 if A[0] == 1 else 1
+
+    l = [0] * m
+    for i in range(len(A)):
+        if A[i] > 0:
+            if l[A[i] - 1] != 1:
+
+                #Changing the value status at the index of our list
+                l[A[i] - 1] = 1
+    for i in range(len(l)):
+
+        #Encountering first 0, i.e, the element with least value
+        if l[i] == 0:
+            return i+1
+            #In case all values are filled between 1 and m
+    return i+2
+
 
 
 def main():
     ary= [3, 4, -1, 1]
-    print firstmissingnum(ary)
+    print solution(ary)
 
 if __name__ == "__main__":
     main()
