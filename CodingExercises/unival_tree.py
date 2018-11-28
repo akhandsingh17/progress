5d15c18aaa227ca12b319c659a7ce8d497383b67
Akhand <akhand17@hotmail.com>
2018-11-28 00:08:17 -0800

    Daily Coding problems

--- CodingExercises/unival_tree.py
+++ CodingExercises/unival_tree.py
@@ -1,70 +1,44 @@
-class Node(object):
+class Node:
     def __init__(self,data):
         self.data=data
         self.left=None
         self.right=None
 
-class BinaryTree(object):
-    def __init__(self,root):
-        self.root=Node(root)
 
-    def preorder(self,start,traverse):
-        if start:
-            traverse= traverse + (str(start.data)+'-')
-            traverse =  self.preorder(start.left,traverse)
-            traverse =  self.preorder(start.right,traverse)
-        return  traverse
+    def countunival(self,root,count):
 
+        if root is None:
+            return True
 
-    def postorder(self,start,traverse):
-        if start:
-            traverse =  self.preorder(start.left,traverse)
-            traverse =  self.preorder(start.right,traverse)
-            traverse= traverse + (str(start.data)+'-')
-        return traverse
+        left = self.countunival(root.left,count)
+        right = self.countunival(root.right,count)
 
-    def inorder(self,start,traverse):
-        if start:
-            traverse =  self.preorder(start.left,traverse)
-            traverse= traverse + (str(start.data)+'-')
-            traverse =  self.preorder(start.right,traverse)
-        return traverse
+        if left == False or right == False:
+            return False
 
-    def getlist(self,str):
-        res=[]
-        for chr in str:
-            if chr != '-':
-                res.append(chr)
-            else:
-                continue
-        return res
+        # if left is not None and data is not matching with root.
+        if root.left and root.left.data != root.data:
+            return False
+
+        if root.right and root.right.data != root.data:
+            return False
+
+        count[0]+=1
+        return True
+
+    def counting(self,root):
+        count=[0]
+        self.countunival(root,count)
+        return count[0]
 
 
-    def print_tree(self,traversetype):
-        if traversetype == 'preorder':
-            print "\n preorder"
-            return self.preorder(self.root,'')
 
-
-        if traversetype == 'postorder':
-            print "\n postorder"
-            return self.postorder(self.root,'')
-
-        if traversetype == 'inorder':
-            print "\n inorder"
-            return self.inorder(self.root,'')
-
-def main():
-
-    tree=BinaryTree(0)
-    tree.root.left=Node(1)
-    tree.root.right=Node(0)
-    tree.root.right.left=Node(1)
-    tree.root.right.right=Node(0)
-    tree.root.right.left.left=Node(1)
-    tree.root.right.left.right=Node(1)
-    path=tree.print_tree('preorder')
-    print tree.getlist(path)
-
-if __name__=='__main__':
-    main()
+root=Node(0)
+root.left=Node(1)
+root.right=Node(0)
+root.right.left=Node(1)
+root.right.right=Node(0)
+root.right.left.left=Node(1)
+root.right.left.right=Node(1)
+## Count Unival Tree's
+print root.counting(root)
