a347d1105c529506890362075feb7c56ce72f9b9
Akhand Singh <akhans@amazon.com>
2019-09-08 20:15:15 -0700

    Doubly Linked List

---
+++ assignments/topics/StacksQueues/DoublyLinkedBase.py
@@ -0,0 +1,41 @@
+class _DoublyLinkedBase:
+    class _Node:
+        def __init__(self, element, prev = None, next= None):
+            self._element = element
+            self._prev = prev
+            self._next = next
+
+    def __init__(self):
+        self._head = self._Node(None)
+        self._tail = self._Node(None)
+        self._head._next = self._tail
+        self.tail._prev = self._head
+        self._size = 0
+
+
+    def __len__(self):
+        return self._size
+
+    def isEmpty(self):
+        return self._size == 0
+
+
+    def insert_between(self, e, predecessor, successor):
+        new = self._Node(e, predecessor, successor)
+        predecessor._next = new
+        successor._prev = new
+        self._size += 1
+        return new
+
+    def delete_node(self, node):
+        predecessor = node._prev
+        successor = node._next
+        predecessor._next = successor
+        successor._prev = predecessor
+        self._size -= 1
+        element = node._element
+        node._element = node._prev = node._next = None
+        return element
+
+
+
