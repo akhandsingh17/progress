fa359b6bb6189e2543f46b8fe2b5607f7ab0ddf3
Akhand Singh <akhans@amazon.com>
2019-12-10 23:22:26 -0800

    refactored changes

--- Topic/Arrays/DiagonalTraverse.py
+++ Topic/Arrays/DiagonalTraverse.py
@@ -13,7 +13,7 @@ Output:  [1,2,4,7,5,3,6,8,9]
 
 """
 
-
+from collections import defaultdict
 class Board:
     " nested non-public class board to maintain a 3x3 matrix "
     def __init__(self):
@@ -30,6 +30,35 @@ class Board:
             raise ValueError('Board position already occupied')
         self._board[i][j] = val
 
+    """Group numbers according to diagonals. Sum of row+col in same diagonal is same"""
+    def diag_traverse(self, m, n):
+        self._rows = m
+        self._cols = n
+        result = []
+        dd = defaultdict(list)
+        for i in range(m):
+            for j in range(n):
+                dd[i+j].append(self._board[i][j])
+
+        for k in sorted(dd.keys()):
+            if k % 2 == 1:
+                dd[k].reverse()
+            result += dd[k]
+        return result
+
+    def zigzag_traverse(self, m, n):
+        self._rows = m
+        self._cols = n
+        for i in range(m):
+            k = 0 if i % 2 == 0 else n - 1
+            for j in range(n):
+                print(self._board[i][k])
+                if i % 2 == 0:
+                    k += 1
+                else:
+                    k -= 1
+
+
 if __name__ == "__main__":
     board = Board()
     board.mark(0, 0, ' 1 ')
@@ -42,3 +71,6 @@ if __name__ == "__main__":
     board.mark(2, 1, ' 8 ')
     board.mark(2, 2, ' 9 ')
     print(board)
+    print('\n')
+    print(board.diag_traverse(3,3))
+    print(board.spiral_traverse(3,3))
